db75e80a1a6164ea07fbc1a20c15b14de7c8b09b
AnilKSahoo <asahoo112@gmail.com>
2020-03-06 19:54:46 +0530

    Change the folder structure and names of variables and functions

--- Check Pallindrome/scripts/main.js
+++ Check Pallindrome/scripts/main.js
@@ -1,19 +1,14 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello World!';
-
 const button = document.querySelector('button');
 const input = document.querySelector('input');
-button.onclick = function() {
+button.onclick = function pallindrome() {
     let str = input.value;
-    let l = str.length;
-    let s = str.toLowerCase();
-    let i;
-    let rev = '';
-    for(i = l-1; i >= 0; i--)
+    let stringLower = str.toLowerCase();
+    let reverse = '';
+    for(let i = str.length-1; i >= 0; i--)
     {
-        rev = rev + s[i];
+        reverse = reverse + s[i];
     }
-    if(rev == s)
+    if(reverse === stringLower)
         document.getElementById("demo").innerHTML = "String is a pallindrome";
     else
         document.getElementById("demo").innerHTML = "String is not a pallindrome";
